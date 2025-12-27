ce3cb473691554b9714ae3f16c42377a62905de8
Jasveer <singhbangajasveer@gmail.com>
2025-12-27 15:54:18 +0530

    2

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
         System.out.println("LUN LE LUN LE LUN LE");
+        int num1=2,num2=5,num3=4,  num5 = 0;
+        num5=num1+num2;
+        num1+=num1+num2;
+        System.out.println(num1);
+        System.out.println(num5);
     }
 }
