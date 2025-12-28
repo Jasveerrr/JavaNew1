25565a54f3413554d441fc56cbd6458638131480
Anish <vu1f2425123@pvppcoe.ac.in>
2025-12-28 13:24:05 +0530

    1st

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,15 @@
 public class Main {
-//    public static void main(String[] args) {
+    public static void main(String[] args) {
 //        System.out.println("Hello Git!");
 //        int num1=2,num2=5,num3=4,  num5 = 0;
 //        num5=num1+num2;
 //        num1+=num1+num2;
 //        System.out.println(num1);
 //        System.out.println(num5);
+int num1 = 4;
+int num= 2;
+int res=num1&num;
+        System.out.println(res);
 
     }
 }
