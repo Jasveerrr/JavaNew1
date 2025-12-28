2a10986d1c9ebe9c56f249ed3009d067260fd38c
Jasveer <singhbangajasveer@gmail.com>
2025-12-28 13:55:46 +0530

    3

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 //        System.out.println("Hello Git!");
+        // ALU OPERATIONS //
+
 //        int num1=2,num2=5,num3=4,  num5 = 0;
 //        num5=num1+num2;
 //        num1+=num1+num2;
 //        System.out.println(num1);
 //        System.out.println(num5);
-        //&&=true or false
-        //&=number
+
+        //--OPERATORS--//
+
+        // &&=true or false//
+        // &=number //
+
         int num1 = 4;
-        int num= 2;
-        int res=num1&num;
-        int res2=num1|num;
+        int num2= 2;
+        int res=num1 & num2;
+        int res2=num1 | num2;
+        int res3=num1 ^ num2;
         System.out.println(res);
         System.out.println(res2);
         System.out.println(~res);
-        System.out.println(7^9);
+        System.out.println(res3);
 
 
     }
