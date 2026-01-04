699b8b108ffc99d50ec3290474f89f953a755ae8
Aniruddh <aniruddhakesarkar6@gmail.>
2026-01-04 13:43:40 +0530

    1st

--- src/Main.java
+++ src/Main.java
@@ -155,7 +155,7 @@ public class Main {
         for (int i = 1; i <= n; i++) {
             if (n % i == 0) {
                 System.out.print(i + " ");
-                System.out.print(Welcome krishna);
+                System.out.print("hello");
 
             }
         }
